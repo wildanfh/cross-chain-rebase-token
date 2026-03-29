60bc40320559f3a38aa4258e5fe5d19fd180b2f4
wildanfh <wildanf2005@gmail.com>
2026-03-29 19:26:58 +0200

    bridging script

---
+++ script/BridgeTokens.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {IRouterClient} from "@ccip/contracts/interfaces/IRouterClient.sol";
+import {Client} from "@ccip/contracts/libraries/Client.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BridgeTokensScript is Script {
+    function run(
+        address receiverAddress,
+        uint64 destinationChainSelector,
+        address tokenToSendAddress,
+        uint256 amountToSend,
+        address linkTokenAddress,
+        address routerAddress
+    ) public {
+        vm.startBroadcast();
+
+        Client.EVMTokenAmount[] memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        tokenAmounts[0] = Client.EVMTokenAmount({
+            token: tokenToSendAddress,
+            amount: amountToSend
+        });
+
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiverAddress),
+            data: "",
+            tokenAmounts: tokenAmounts,
+            feeToken: linkTokenAddress,
+            extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({gasLimit: 0}))
+        });
+
+        uint256 ccipFee = IRouterClient(routerAddress).getFee(destinationChainSelector, message);
+
+        IERC20(linkTokenAddress).approve(routerAddress, ccipFee);
+        IERC20(tokenToSendAddress).approve(routerAddress, amountToSend);
+
+        IRouterClient(routerAddress).ccipSend(destinationChainSelector, message);
+
+        vm.stopBroadcast();
+    }
+}
