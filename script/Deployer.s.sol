a92f9ed73a80746d699e8216196b99417906b85e
wildanfh <wildanf2005@gmail.com>
2026-03-29 18:57:28 +0200

    token and pool deployer

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,9 +4,41 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {IERC20} from "@openzeppelin/contracts@5.3.0/token/ERC20/IERC20.sol";
+import {RegistryModuleOwnerCustom} from "@ccip/contracts/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {TokenAdminRegistry} from "@ccip/contracts/tokenAdminRegistry/TokenAdminRegistry.sol";
 
 contract TokenAndPoolDeployer is Script {
-    // Implementation for RebaseToken and RebaseTokenPool deployment will be added here.
+    function run() public returns (RebaseToken token, RebaseTokenPool pool) {
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+
+        vm.startBroadcast();
+        token = new RebaseToken();
+        pool = new RebaseTokenPool(
+            IERC20(address(token)),
+            token.decimals(),
+            address(0),
+            networkDetails.rmnProxyAddress,
+            networkDetails.routerAddress
+        );
+
+        token.grantMintAndBurnRole(address(pool));
+
+        RegistryModuleOwnerCustom(networkDetails.registryModuleOwnerCustomAddress)
+            .registerAdminViaOwner(address(token));
+
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress)
+            .acceptAdminRole(address(token));
+
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress)
+            .setPool(address(token), address(pool));
+
+        vm.stopBroadcast();
+    }
 }
 
 contract VaultDeployer is Script {
